f4a3569d883ebf58fd90320c65b0cde4b2e3239a
María José Rodríguez F <rodriguez.mariajose.f@gmail.com>
2017-05-24 11:10:48 -0400

    avances

---
+++ assets/js/main.js
@@ -0,0 +1,53 @@
+function Snake(){
+	this.x = x;
+	this.y = y;
+	}
+
+var 
+var x= document.getElementsByClassName("cor1").value;
+var y = document.getElementsByClassName("cor2").value;
+tablero[x][y] = "A";
+
+//Defino el tablero
+var tablero = [
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+  [0,0,0,0,0,0,0,0,0,0],
+]
+//Ingreso el auto en la posicion 3,5
+
+
+//Obtengo el div en donde se dibujará el tablero
+var divTablero = document.getElementById("tablero");
+
+//Variables temporales
+var fila, casilla;
+
+//Recorro el arreglo para dibujar el tablero
+for(var i = 0; i<tablero.length; i++){
+  fila = document.createElement("div");
+  fila.classList.add("fila");
+  for(var j = 0; j<tablero[i].length; j++){
+    casilla = document.createElement("div");
+    casilla.innerHTML = tablero[i][j];
+    casilla.classList.add("casilla")
+    fila.appendChild(casilla);
+  }
+  divTablero.appendChild(fila);
+}
+
+var teclado = {
+    Up:38,
+    Down:40,
+    Left:37,
+    Right:39,
+}
+
+document.addEventListener("keyup", dibujar teclado);
+function dibujarTeclado(evento)
+{
+    console.log(evento.keyCode);
+}
